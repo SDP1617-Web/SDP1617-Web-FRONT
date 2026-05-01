9561cf106593daa172c296ef9cf52b4f7aa121f4
choichangyeon <changyon99@naver.com>
2026-05-01 19:47:09 +0900

    feat: 게시물 관련 API Skeleton 코드 작성

---
+++ src/app/api/post/route.ts
@@ -0,0 +1,20 @@
+import { NextResponse } from "next/server";
+import { ENV_CONSTANTS } from "@/constants/EnvConstants";
+
+const API_URL = ENV_CONSTANTS.API_URL;
+
+// TODO: 관련 API 엔드포인트 수정이 필요할 수 있음
+export const GET = async (request: Request) => {
+    const response = await fetch(`${API_URL}/posts`); 
+    const data = await response.json();
+    return NextResponse.json(data);
+};
+
+export const POST = async (request: Request) => {
+    const response = await fetch(`${API_URL}/posts`, {
+        method: 'POST',
+        body: JSON.stringify(request.body),
+    });
+    const data = await response.json();
+    return NextResponse.json(data);
+};
