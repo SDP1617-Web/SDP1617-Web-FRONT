96c2c211b71f062d22ce6e7ef0c9e3bda41ab1f1
Lee Hyeon Seung <arale3488@gmail.com>
2026-05-15 14:59:33 +0900

    feat: 파트너 섹션 구현 및 페이지 연결

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,11 +1,25 @@
 // 홈화면
 
-import HeroSection1 from "@/components/home/HeroSection1";
+import HeroSection1 from "@/components/home/sections/HeroSection1";
+import IntroSection from "@/components/home/sections/IntroSection";
+import GoalsSection from "@/components/home/sections/GoalsSection";
+import StatsSection from "@/components/home/sections/StatsSection";
+import ProjectsSection from "@/components/home/sections/ProjectsSection";
+import ActivitiesSection from "@/components/home/sections/ActivitiesSection";
+import FaqSection from "@/components/home/sections/FaqSection";
+import PartnersSection from "@/components/home/sections/PartnersSection";
 
 export default function Home() {
   return (
     <>
       <HeroSection1 />
+      <IntroSection />
+      <GoalsSection />
+      <StatsSection />
+      <ProjectsSection />
+      <ActivitiesSection />
+      <FaqSection />
+      <PartnersSection />
     </>
   );
 }
---
+++ src/components/home/sections/PartnersSection.tsx
@@ -0,0 +1,42 @@
+import Image from "next/image";
+
+const PARTNERS = [
+  { src: "/partner1.svg", alt: "partner1" },
+  { src: "/partner2.svg", alt: "partner2" },
+  { src: "/partner3.svg", alt: "partner3" },
+  { src: "/partner4.svg", alt: "partner4" },
+  { src: "/partner5.svg", alt: "partner5" },
+];
+
+export default function PartnersSection() {
+  return (
+    <section
+      style={{ marginTop: "191.25px", marginBottom: "82px" }}
+      className="flex justify-center"
+    >
+      <div style={{ width: "1202px" }} className="flex flex-col">
+        {/* 타이틀 */}
+        <p className="h2" style={{ color: "var(--color-sdp-grey-900)" }}>
+          SDP 는 기관·학계·국제기구와 함께합니다.
+        </p>
+
+        {/* 로고 */}
+        <div
+          className="flex flex-row items-center"
+          style={{ marginTop: "48px", gap: "61px" }}
+        >
+          {PARTNERS.map((partner) => (
+            <Image
+              key={partner.src}
+              src={partner.src}
+              alt={partner.alt}
+              width={0}
+              height={0}
+              style={{ width: "auto", height: "auto" }}
+            />
+          ))}
+        </div>
+      </div>
+    </section>
+  );
+}
