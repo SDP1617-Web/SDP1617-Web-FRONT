1f48cd3108b90b17c8d04302793788203d23049e
choichangyeon <changyon99@naver.com>
2026-05-22 16:39:17 +0900

    feat: apply form 기본 구현

--- src/components/apply/ApplyForm.tsx
+++ src/components/apply/ApplyForm.tsx
@@ -1,5 +1,85 @@
 const ApplyForm = () => {
-  return <form action=""></form>
+  return (
+    <form action="" className="h-[1000px] w-full px-[360px] pt-[90px]">
+      {/* 지원자 정보 */}
+      <section className="flex flex-col">
+        <div className="flex flex-row items-center gap-[20px]">
+          {/* Button Component - v4 */}
+          <div className="bg-sdp-main-secondary inline-flex size-11 items-center justify-center rounded-full">
+            <h3 className="h3 text-sdp-grey-900 justify-center font-semibold">
+              1
+            </h3>
+          </div>
+          <h3 className="h3 text-sdp-grey-900">{APPLY_INFORMATION}</h3>
+        </div>
+        {/* 개인정보 입력 */}
+        <div className="mt-[50px] flex flex-row gap-x-[25px]">
+          <div className="flex flex-col gap-y-[32px]">
+            <div className="flex flex-col gap-y-[16px]">
+              <h3 className="h3 font-semibold">{APPLY_NAME}</h3>
+              {/* Input Component */}
+            </div>
+            <div className="flex flex-col gap-y-[16px]">
+              <h3 className="h3 font-semibold">{APPLY_BIRTH}</h3>
+              {/* Input Component */}
+            </div>
+          </div>
+          <div className="flex flex-col gap-y-[32px]">
+            <div className="flex flex-col gap-y-[16px]">
+              <h3 className="h3 font-semibold">{APPLY_CONTRACT}</h3>
+              {/* Input Component */}
+            </div>
+            <div className="flex flex-col gap-y-[16px]">
+              <h3 className="h3 font-semibold">{APPLY_EMAIL}</h3>
+              {/* Input Component */}
+            </div>
+          </div>
+        </div>
+      </section>
+
+      {/* 지원 부서 선택 */}
+      <section className="mt-[150px] flex flex-col gap-x-[16px]">
+        <h3 className="h3 font-semibold">{APPLY_TEAM}</h3>
+        {/* Button Component - v3 */}
+        <div className="flex flex-row gap-x-[23px]"></div>
+      </section>
+
+      {/* 포트폴리오 */}
+      <section className="mt-[120px] flex flex-col gap-x-[16px]">
+        <h3 className="h3 font-semibold">{APPLY_PORTFOLIO}</h3>
+        {/* pdf 입력 폼 */}
+        <div className="outline-sdp-grey-300 inline-flex flex-col items-center justify-start gap-[8px] self-stretch rounded-[20px] p-[32px] outline-2 -outline-offset-2">
+          {/* pdf 이미지 */}
+          <div className="size-12 justify-center text-center font-['Font_Awesome_5_Free'] text-5xl leading-[47.76px] font-black text-neutral-300">
+            
+          </div>
+          <div className="flex h-14 flex-col items-center justify-start self-stretch pt-2">
+            <h4 className="h4 text-sdp-grey-500 justify-center text-center font-medium">
+              {APPLY_FILE_TEXT}
+            </h4>
+          </div>
+          <div className="inline-flex h-16 items-center justify-center rounded-[20px] bg-neutral-100 px-8 py-4">
+            {/* Button Component - v6 */}
+            <div className="justify-center text-center">
+              {APPLY_FILE_SELECT}
+            </div>
+          </div>
+        </div>
+      </section>
+    </form>
+  )
 }
+
+const APPLY_INFORMATION = '지원자 정보'
+const APPLY_NAME = '이름'
+const APPLY_BIRTH = '생년월일'
+const APPLY_CONTRACT = '연락처'
+const APPLY_EMAIL = '이메일'
+
+const APPLY_TEAM = '지원 부서 선택'
+
+const APPLY_PORTFOLIO = '포트폴리오'
+const APPLY_FILE_TEXT = 'PDF 또는 PPT 파일 첨부'
+const APPLY_FILE_SELECT = '파일 선택'
 
 export default ApplyForm
