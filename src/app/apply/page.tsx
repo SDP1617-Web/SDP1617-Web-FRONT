95a4a4318559c65281da00f349399ed2bf857a59
choichangyeon <changyon99@naver.com>
2026-05-22 16:38:34 +0900

    feat: apply form 추가

--- src/app/apply/page.tsx
+++ src/app/apply/page.tsx
@@ -1,12 +1,14 @@
 //지원하기 페이지
 'use client'
 import ApplyHeading from '@/components/apply/ApplyHeading'
+import ApplyForm from '@/components/apply/ApplyForm'
 
 const ApplyPage = () => {
   return (
-    <div>
+    <>
       <ApplyHeading />
-    </div>
+      <ApplyForm />
+    </>
   )
 }
 
