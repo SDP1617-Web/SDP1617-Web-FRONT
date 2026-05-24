ef2b82f274bb3dd329aa9b1e62fe2fc9590b71af
choichangyeon <changyon99@naver.com>
2026-05-24 11:07:59 +0900

    feat: 좌우대칭 수정

--- src/components/apply/ApplyHeading.tsx
+++ src/components/apply/ApplyHeading.tsx
@@ -2,7 +2,7 @@ import Image from 'next/image'
 
 const ApplyHeading = () => {
   return (
-    <div className="relative flex h-96 w-full flex-row">
+    <div className="relative flex h-96 w-full flex-row overflow-hidden">
       <div className="from-sdp-main-primary to-sdp-grey-900 absolute left-[-110px] h-full w-[calc(50%+110px)] bg-linear-to-r to-20%">
         <div className="flex flex-col pt-[174px] pl-[410px]">
           <h1 className="h1 text-sdp-grey-50 flex justify-items-start">
