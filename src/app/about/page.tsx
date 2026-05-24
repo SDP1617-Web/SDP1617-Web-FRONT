061862ed5505be5aaa4fdd66f51bb89aeb185e38
Lee Hyeon Seung <arale3488@gmail.com>
2026-05-24 15:26:25 +0900

    fix: about 페이지 eclipses 위치를 경계 마커 방식으로 수정

--- src/app/about/page.tsx
+++ src/app/about/page.tsx
@@ -1,30 +1,26 @@
-import Image from "next/image";
-import AboutHero from "@/components/about/AboutHero";
-import AboutIntro from "@/components/about/AboutIntro";
-import AboutDark from "@/components/about/AboutDark";
-import TalentSection from "@/components/about/TalentSection";
-import DepartmentSection from "@/components/about/DepartmentSection";
+import Image from 'next/image'
+import AboutHero from '@/components/about/AboutHero'
+import AboutIntro from '@/components/about/AboutIntro'
+import AboutDark from '@/components/about/AboutDark'
+import TalentSection from '@/components/about/TalentSection'
+import DepartmentSection from '@/components/about/DepartmentSection'
 
 export default function AboutPage() {
   return (
     <main className="flex-1">
       <AboutHero />
-      <div className="relative">
-        <AboutIntro />
-        <div className="pointer-events-none absolute left-1/2 top-[var(--intro-height)] z-10 -translate-x-1/2 -translate-y-1/2">
-          <Image
-            src="/eclipses.svg"
-            alt=""
-            width={23}
-            height={222}
-            priority
-          />
+      <AboutIntro />
+
+      {/* AboutIntro와 AboutDark의 경계. 높이 0인 기준점 */}
+      <div className="relative z-10 h-0">
+        <div className="pointer-events-none absolute top-0 left-1/2 -translate-x-1/2 -translate-y-1/2">
+          <Image src="/eclipses.svg" alt="" width={23} height={222} />
         </div>
-        <AboutDark />
       </div>
 
+      <AboutDark />
       <TalentSection />
       <DepartmentSection />
     </main>
-  );
+  )
 }
