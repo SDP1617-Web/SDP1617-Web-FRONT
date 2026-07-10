bbcaac6c1aeb1216086df7506b46cab53aff99b7
choichangyeon <changyon99@naver.com>
2026-07-10 21:31:15 +0900

    refactor: 렌더링 오류 수정

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,25 +1,28 @@
-import type { Metadata } from "next";
-import "./globals.css";
-import Header from "@/components/layout/Header";
-import Footer from "@/components/layout/Footer";
+import type { Metadata } from 'next'
+import './globals.css'
+import Header from '@/components/layout/Header'
+import Footer from '@/components/layout/Footer'
 
 export const metadata: Metadata = {
-  title: "SDP | Sustainable Development Program",
-  description: "학부생 주도로 설립된 한국 최초의 지속가능성 관련 글로벌 리더십 프로그램",
-};
+  title: 'SDP | Sustainable Development Program',
+  description:
+    '학부생 주도로 설립된 한국 최초의 지속가능성 관련 글로벌 리더십 프로그램',
+}
 
 export default function RootLayout({
   children,
 }: Readonly<{
-  children: React.ReactNode;
+  children: React.ReactNode
 }>) {
   return (
-    <html lang="ko" className="h-full antialiased">
-      <body className="min-h-full flex flex-col">
-        <Header />
-        {children}
-        <Footer />
-      </body>
-    </html>
-  );
+    <>
+      <html lang="ko" className="h-full antialiased">
+        <body className="flex min-h-full flex-col">
+          <Header />
+          {children}
+          <Footer />
+        </body>
+      </html>
+    </>
+  )
 }
